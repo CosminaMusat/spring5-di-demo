fd755e17bcb4dc243362be086e80f60924059435
Musat Andreea Cosmina <andreea.musat@orange.com>
2019-08-13 14:59:55 +0300

    Property source

--- src/main/java/guru/springframework/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/config/GreetingServiceConfig.java
@@ -8,35 +8,33 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Profile;
 
-/**
- * Created by jt on 5/25/17.
- */
 @Configuration
 public class GreetingServiceConfig {
 
     @Bean
-    GreetingServiceFactory greetingServiceFactory(GreetingRepository repository){
-        return new GreetingServiceFactory(repository);
+    GreetingServiceFactory greetingServiceFactory(GreetingRepository greetingRepository) {
+        return new GreetingServiceFactory(greetingRepository);
     }
 
     @Bean
     @Primary
     @Profile({"default", "en"})
-    GreetingService primaryGreetingService(GreetingServiceFactory greetingServiceFactory){
+    GreetingService primaryGreetingService(GreetingServiceFactory greetingServiceFactory ) {
         return greetingServiceFactory.createGreetingService("en");
     }
 
     @Bean
     @Primary
     @Profile("es")
-    GreetingService primarySpanishGreetingService(GreetingServiceFactory greetingServiceFactory){
+    GreetingService primarySpanishGreetingService(GreetingServiceFactory greetingServiceFactory ) {
         return greetingServiceFactory.createGreetingService("es");
     }
 
     @Bean
     @Primary
     @Profile("de")
-    GreetingService primaryGermanGreetingService(GreetingServiceFactory greetingServiceFactory){
+    GreetingService primaryGermanGreetingService(GreetingServiceFactory greetingServiceFactory ) {
         return greetingServiceFactory.createGreetingService("de");
     }
+
 }
